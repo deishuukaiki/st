9154dd0134a192424f7aa910f3ab86e3648d19b0
Maciej Sobierski <maciek54134@tutanota.com>
2021-10-24 22:30:42 +0200

    changed font

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "Nerd Font Complete:pixelsize=12:antialias=true:autohint=true","devicons-regular" };
+static char *font = "SauceCodePro Nerd Font:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "Symbola:pixelsize=12:antialias=true:autohint=true","devicons-regular" };
 static int borderpx = 0;
 
 /*
